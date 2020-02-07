66d3998e4870eee6920a7326a86f01dd58f65d06
surya474 <gantasuryateja474@gmail.com>
2020-02-07 18:14:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import './App.css';
+
+function App() {
+  return (
+   <Provider>
+       <Fragment>
+         
+
+       </Fragment>
+
+   </Provider>
+  );
+}
+
+export default App;
---
+++ src/components/Router/appRouter.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+
+export const AppRouter = ()=>{
+
+    <BrowserRouter>
+    <Switch>
+                <Route path='/' component={} exact={true} />
+                <Route path='/favourites' component={FavoriteQuotesPage} />
+               
+            </Switch>
+    
+    </BrowserRouter>
+
+}
